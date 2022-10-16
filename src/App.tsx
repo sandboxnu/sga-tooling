74dda56693145d98646827b7b64d38c7081c3801
Ally Chao <73146562+allychao@users.noreply.github.com>
2022-10-16 15:44:24 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useState } from 'react';
 import logo from './logo.svg';
+import Footer from './components/Footer';
 import './App.css';
 import Menu from './components/Menu';
 import LoginPage from './components/LoginPage';
@@ -21,8 +22,6 @@ function App() {
     <LoginContext.Provider value={{user, setUser}}>
       <LoginPage />
     </LoginContext.Provider>
-  
-
   );
 }
 
