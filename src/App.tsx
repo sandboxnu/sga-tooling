3ed53efa3b30ea3d5042a51f516fe757dcd14ec4
Ally Chao <73146562+allychao@users.noreply.github.com>
2022-10-23 16:57:47 -0400

    merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import logo from './logo.svg';
+import Footer from './components/Footer';
 import './App.css';
+import Menu from './components/Menu';
+import LoginPage from './components/LoginPage';
+
+export type User = number | null
+
+type UserContext = {
+  user: User,
+  setUser: React.Dispatch<React.SetStateAction<User>>
+}
+
+export const LoginContext = createContext<UserContext>({} as UserContext);
 
 function App() {
+
+  const [user, setUser] = useState<User | null>(null)
+
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
     <LoginContext.Provider value={{user, setUser}}>
       <LoginPage />
     </LoginContext.Provider>
-=======
-    <Footer />
->>>>>>> fb6e5fc (initial commit)
-=======
-    <Footer />
->>>>>>> fb6e5fc (initial commit)
   );
 }
 
