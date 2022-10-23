62b287dd729988f48faba9bfa7a389065c866152
Ally Chao <73146562+allychao@users.noreply.github.com>
2022-10-23 17:55:08 -0400

    more removing irrelevant code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        'sgared': '#D41B2C',
-        'sgared-hover': '#b51727',
-        'sgared-active': '#87121d',
-        'transparent-gray': '#ECECEC52',
-      },
-    }
+    extend: {}
   },
   plugins: [],
 }
