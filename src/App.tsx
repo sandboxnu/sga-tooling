e9d75bbb529bacf2cf2500573b9daad40f44f900
banushi-a <altonbanushi@icloud.com>
2022-10-23 15:58:00 -0400

    Added mock tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Menu from './components/Menu';
+import LoginPage from './components/LoginPage';
+
+export type User = number | null
+
+type UserContext = {
+  user: User,
+  setUser: React.Dispatch<React.SetStateAction<User>>
+}
+
+export const LoginContext = createContext<UserContext>({} as UserContext);
 
 function App() {
-  return <Menu />;
+
+  const [user, setUser] = useState<User | null>(null)
+
+  return (
+    <LoginContext.Provider value={{user, setUser}}>
+      <LoginPage />
+    </LoginContext.Provider>
+  
+
+  );
 }
 
 export default App;
