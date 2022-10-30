3579c9c56b9d01d4f53a9bf82bf96365bb7b018c
Raveesh Mehta <raveeshnmehta@gmail.com>
2022-10-30 16:03:26 -0400

    Centers text vertically
    Co-authored-by: Melody Liu <melocule@users.noreply.github.com>

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -8,9 +8,9 @@ import { ExclamationCircleIcon } from '@heroicons/react/24/solid'
  */
 const Alert = (props: {message: string, className?: string}): ReactElement =>  {
     return (
-        <div className={`flex items-start space-x-2 text-left bg-[#FFEDC0] rounded-lg p-3 text-sm ${props.className}`} role="alert">
-            <ExclamationCircleIcon className="w-12 fill-[#FEC12F] stroke-white"/>
-            <span className="font-medium">{props.message}</span>
+        <div className={`flex items-start space-x-2 text-left bg-[#FFEDC0] rounded-lg p-3 text-sm h-fit ${props.className}`} role="alert">
+            <ExclamationCircleIcon className="w-12 fill-[#FEC12F] flex-none stroke-white"/>
+            <span className="font-medium self-center">{props.message}</span>
         </div>
     );
 }
