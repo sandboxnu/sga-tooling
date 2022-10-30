dc9d3bcc8bb4a6b6a9819507bf29bdcc4fbe4a8b
Raveesh Mehta <raveeshnmehta@gmail.com>
2022-10-30 16:19:14 -0400

    Adds and uses colors in theme config

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -8,8 +8,8 @@ import { ExclamationCircleIcon } from '@heroicons/react/24/solid'
  */
 const Alert = (props: {message: string, className?: string}): ReactElement =>  {
     return (
-        <div className={`flex items-start space-x-2 text-left bg-[#FFEDC0] rounded-lg p-3 text-sm h-fit ${props.className}`} role="alert">
-            <ExclamationCircleIcon className="w-12 fill-[#FEC12F] flex-none stroke-white"/>
+        <div className={`flex items-start space-x-2 text-left bg-warning-light rounded-lg p-3 text-sm h-fit ${props.className}`} role="alert">
+            <ExclamationCircleIcon className="w-12 fill-warning-dark flex-none stroke-white"/>
             <span className="font-medium self-center">{props.message}</span>
         </div>
     );
