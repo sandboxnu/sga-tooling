7115a23897cab444e19f621262fac802a9eaf0c5
Raveesh Mehta <raveeshnmehta@gmail.com>
2022-11-01 20:42:32 -0400

    Removes extraneous file/code

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,6 @@ import logo from './logo.svg';
 import Footer from './components/Footer';
 import './App.css';
 import Menu from './components/Menu';
-import LoginPage from './components/LoginPage';
-
-export type User = number | null
-
-type UserContext = {
-  user: User,
-  setUser: React.Dispatch<React.SetStateAction<User>>
-}
-
-export const LoginContext = createContext<UserContext>({} as UserContext);
 
 function App() {
 
--- src/requests.tsx
+++
@@ -1,9 +0,0 @@
-export type UserData = {};
-
-export async function mockLoginAPI(id: Number):Promise<UserData> {
-    const response = await fetch(`/persons/${id}`);
-    
-    return response;
-}
-
-module.exports = mockLoginAPI;
