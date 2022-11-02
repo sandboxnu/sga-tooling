43759a258a389e2defefed8ee3bf77156757a0cd
Alton Banushi <89368127+banushi-a@users.noreply.github.com>
2022-11-01 21:04:35 -0400

    Tests are working (#4)
    * Added mock tests
    
    * Tests are working
    
    * Moved to new branch
    
    * Modified config files to work with automatic github tests
    
    * node issue resolved (hopefully)
    
    * add lint
    
    * test finally working please
    
    * Implemented most of the suggestions
    
    * Changed based on PR Comments
    
    * linting errors

---
+++ src/requests.tsx
@@ -0,0 +1,9 @@
+export type UserData = {};
+
+export async function getUserInfo(id: Number):Promise<UserData> {
+    const response = await fetch(`/persons/${id}`);
+    
+    return response;
+}
+
+module.exports = getUserInfo;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'sgared': '#D41B2C',
+        'sgared-hover': '#b51727',
+        'sgared-active': '#87121d',
+        'transparent-gray': '#ECECEC52',
+        'alert-yellow': '#FFEDC0'
+      },
+    }
   },
   plugins: [],
 }
