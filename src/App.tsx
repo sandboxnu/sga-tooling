a632834e4a88220e28aec8355f1e8bbcbcc82493
Alton Banushi <89368127+banushi-a@users.noreply.github.com>
2022-11-01 21:04:35 -0400

    Tests are working (#4)
    * Added mock tests
    
    * Tests are working
    
    * Moved to new branch
    
    * Modified config files to work with automatic github tests
    
    * node issue resolved (hopefully)
    
    * add lint
    
    * test finally working please
    
    * Implemented most of the suggestions
    
    * Changed based on PR Comments
    
    * linting errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,44 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import logo from "./logo.svg";
 import Footer from "./components/Footer";
 import "./App.css";
 import Menu from "./components/Menu";
+import LoginPage from "./components/LoginPage";
 import EventCard from "./components/EventCard";
 
+export type User = number | null;
+
 function App() {
+  const [user, setUser] = useState<User | null>(null);
   return (
     <>
-      <div className="flex min-h-screen flex-col justify-start">
-        <Menu />
-        <div className="flex flex-row">
-          <div className="w-1/5 md:w-[10%]">placeholder for dates whee</div>
-          <EventCard
-            startTime={new Date()}
-            name="Sample Event 1"
-            location="WVF 020"
-            description="Sample text. Please don’t read this area. If you do you may be subject to legal action. The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal."
-            live={false}
-          />
+      <LoginContext.Provider value={{ user, setUser }}>
+        <div className="flex min-h-screen flex-col justify-start">
+          <LoginPage />
+          <div className="flex flex-row">
+            <div className="w-1/5 md:w-[10%]">placeholder for dates whee</div>
+            <EventCard
+              startTime={new Date()}
+              name="Sample Event 1"
+              location="WVF 020"
+              description="Sample text. Please don’t read this ar ea. If you do you may be subject to legal action. The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal."
+              live={false}
+            />
+            {/*<Menu/>*/}
+          </div>
+          <div className="flex flex-row">
+            <div className="w-1/5 md:w-[10%]">this be the live event</div>
+            <EventCard
+              startTime={new Date()}
+              name="Sample Event 2"
+              location="Afterhours, Curry Student Center"
+              description="The organizer of this event has chosen to notify you about it. The agenda is to vibe to Lil Nas X."
+              live={true}
+            />
+          </div>
+          <Footer />
         </div>
-        <div className="flex flex-row">
-          <div className="w-1/5 md:w-[10%]">this be the live event</div>
-          <EventCard
-            startTime={new Date()}
-            name="Sample Event 2"
-            location="Afterhours, Curry Student Center"
-            description="The organizer of this event has chosen to notify you about it. The agenda is to vibe to Lil Nas X."
-            live={true}
-          />
-        </div>
-        <Footer />
-      </div>
+      </LoginContext.Provider>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'sga-red': '#D41B2C',
-        'sga-red-hover': '#b51727',
-        'sga-red-active': '#87121d',
-        'transparent-gray': '#ECECEC52',
+        "sga-red": "#D41B2C",
+        "sga-red-hover": "#b51727",
+        "sga-red-active": "#87121d",
+        "transparent-gray": "#ECECEC52",
+        "alert-yellow": "#FFEDC0",
       },
       fontFamily: {
-        'montserrat': ['Montserrat'],
-        'sans': ['"Open Sans"'],
+        montserrat: ["Montserrat"],
+        sans: ['"Open Sans"'],
       },
     },
   },
   plugins: [],
-}
+};
