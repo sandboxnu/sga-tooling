63a8c4e577610eb7133b6648fd4bfef2b21a80b1
chanmioh <chanmioh@gmail.com>
2022-11-03 17:45:10 -0400

    Fixed indentation issues

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,50 +3,50 @@ import ".././styles.css";
 
 // Footer with source code and project information that goes at the bottom of each page
 const Footer = (): ReactElement => {
-	return (
-		<div className="w-full px-2 py-4 mt-4 flex flex-row justify-between border-t-2 absolute bottom-0">
-			<div>
-				Made by students @&nbsp;
-				<a
-					href="https://sandboxnu.com"
-					className="link"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Sandbox
-				</a>
-				. Source on&nbsp;
-				<a
-					href="https://github.com/sandboxnu/sga-tooling"
-					className="link"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					GitHub.
-				</a>
-			</div>
+  return (
+    <div className="w-full px-2 py-4 mt-4 flex flex-row justify-between border-t-2 absolute bottom-0">
+      <div>
+        Made by students @&nbsp;
+        <a
+          href="https://sandboxnu.com"
+          className="link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sandbox
+        </a>
+        . Source on&nbsp;
+        <a
+          href="https://github.com/sandboxnu/sga-tooling"
+          className="link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub.
+        </a>
+      </div>
 
-			<div>
-				<a
-					href="https://github.com/sandboxnu/office-hours/discussions"
-					className="link"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Give us feedback
-				</a>
-				<span> or </span>
-				<a
-					href="https://github.com/sandboxnu/office-hours/issues"
-					className="link"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					File a bug report
-				</a>
-			</div>
-		</div>
-	);
+      <div>
+        <a
+          href="https://github.com/sandboxnu/office-hours/discussions"
+          className="link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Give us feedback
+        </a>
+        <span> or </span>
+        <a
+          href="https://github.com/sandboxnu/office-hours/issues"
+          className="link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          File a bug report
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,52 +1,52 @@
-import React, { ReactElement, useContext, useState } from "react";
+import { ReactElement, useContext, useState } from "react";
 import { LoginContext } from "../App";
 // import { getUserInfo } from '../requests';
 
 const LoginPage = (): ReactElement => {
-	const { setUser } = useContext(LoginContext);
-	const [input, setInput] = useState(""); // value is the value that the user entered
+  const { setUser } = useContext(LoginContext);
+  const [input, setInput] = useState(""); // value is the value that the user entered
 
-	function login() {
-		if (input) {
-			localStorage.setItem("user", JSON.stringify(`${input}`));
-			setUser(parseInt(input));
-		} else {
-			alert("Error: Invalid NUID");
-		}
-	}
+  function login() {
+    if (input) {
+      localStorage.setItem("user", JSON.stringify(`${input}`));
+      setUser(parseInt(input));
+    } else {
+      alert("Error: Invalid NUID");
+    }
+  }
 
-	return (
-		<div>
-			<div className="flex flex-col justify-end min-h-[68vh] bg-gradient-to-r from-red-300 to-gray-300">
-				<div>
-					<form className="flex-col px-8 py-5 bg-transparent-gray rounded-tl-lg rounded-tr-lg shadow-xl">
-						<input
-							value={input}
-							type="text"
-							id="nuid-entry"
-							onChange={(e) => setInput(e.target.value)}
-							className="w-full bg-gray-50 border border-black text-black text-xl rounded-lg focus:ring-black-500 focus:border-black-500 block px-2.5 py-4 my-4"
-							placeholder="NUID"
-							required
-						/>
-						<button
-							onClick={(e) => login()}
-							className="w-full my-2.5 bg-sgared text-white text-2xl font-semibold rounded-lg px-2.5 py-4 hover:bg-sgared-hover active:bg-sgared-active"
-						>
-							Log In
-						</button>
-					</form>
-				</div>
-			</div>
-			<div className="flex justify-center align-center">
-				<img
-					className="max-w-[100%] max-h-[100%]"
-					src="https://giving.northeastern.edu/live/image/gid/2/width/1260/height/630/crop/1/src_region/294,25,1751,1483/484_Club_-_Student_Government_Association.jpg"
-					alt="Student Government Association Logo"
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="flex flex-col justify-end min-h-[68vh] bg-gradient-to-r from-red-300 to-gray-300">
+        <div>
+          <form className="flex-col px-8 py-5 bg-transparent-gray rounded-tl-lg rounded-tr-lg shadow-xl">
+            <input
+              value={input}
+              type="text"
+              id="nuid-entry"
+              onChange={(e) => setInput(e.target.value)}
+              className="w-full bg-gray-50 border border-black text-black text-xl rounded-lg focus:ring-black-500 focus:border-black-500 block px-2.5 py-4 my-4"
+              placeholder="NUID"
+              required
+            />
+            <button
+              onClick={(e) => login()}
+              className="w-full my-2.5 bg-sgared text-white text-2xl font-semibold rounded-lg px-2.5 py-4 hover:bg-sgared-hover active:bg-sgared-active"
+            >
+              Log In
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className="flex justify-center align-center">
+        <img
+          className="max-w-[100%] max-h-[100%]"
+          src="https://giving.northeastern.edu/live/image/gid/2/width/1260/height/630/crop/1/src_region/294,25,1751,1483/484_Club_-_Student_Government_Association.jpg"
+          alt="Student Government Association Logo"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
