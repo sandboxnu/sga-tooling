2d512c11636a47b8c3457bff44d8fbdf30af5329
chanmioh <chanmioh@gmail.com>
2022-11-03 16:51:24 -0400

    Fixed linting issues

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,51 +1,52 @@
 import React from "react";
 import { ReactElement } from "react";
-import '.././styles.css';
+import ".././styles.css";
 
 // Footer with source code and project information that goes at the bottom of each page
 const Footer = (): ReactElement => {
-  return (
-    <div className="w-full px-2 py-4 mt-4 flex flex-row justify-between border-t-2 absolute bottom-0">
-      <div>
-        Made by students @{" "}
-        <a className="link"
-          onClick={() => {
-            window.open("https://sandboxnu.com");
-          }}
-        >
-          Sandbox
-        </a>
-        . Source on{" "}
-        <a className="link"
-          onClick={() => {
-            window.open("https://github.com/sandboxnu/sga-tooling");
-          }}
-        >
-          GitHub.
-        </a>
-      </div>
+	return (
+		<div className="w-full px-2 py-4 mt-4 flex flex-row justify-between border-t-2 absolute bottom-0">
+			<div>
+				Made by students @
+				<a
+					href="https://sandboxnu.com"
+					className="link"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Sandbox. Source on
+				</a>
+				<a
+					href="https://github.com/sandboxnu/sga-tooling"
+					className="link"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					GitHub.
+				</a>
+			</div>
 
-      <div>
-        <a className="link"
-          onClick={() => {
-            window.open(
-              "https://github.com/sandboxnu/office-hours/discussions"
-            );
-          }}
-        >
-          Give us feedback
-        </a>
-        <span> or </span>
-        <a className="link"
-          onClick={() => {
-            window.open("https://github.com/sandboxnu/office-hours/issues");
-          }}
-        >
-          File a bug report
-        </a>
-      </div>
-    </div>
-  );
+			<div>
+				<a
+					href="https://github.com/sandboxnu/office-hours/discussions"
+					className="link"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Give us feedback
+				</a>
+				<span> or </span>
+				<a
+					href="https://github.com/sandboxnu/office-hours/issues"
+					className="link"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					File a bug report
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,36 +1,57 @@
-import React, { ReactElement } from 'react';
-import { Disclosure, Transition } from '@headlessui/react'
+import React, { ReactElement } from "react";
+import { Disclosure, Transition } from "@headlessui/react";
 
 /**
  * Creates a navbar menu with the SGA logo and a predetermined
  * list of quick links that collapse into a hamburger menu.
- * 
+ *
  * @returns The navbar in a div
  */
-const Menu = (): ReactElement =>  {
-    const navigation = ['Calendar', 'Events', 'Attendance'];
+const Menu = (): ReactElement => {
+	const navigation = ["Calendar", "Events", "Attendance"];
 
-    return (
-        <div className="flex items-center justify-between space-x-16 md:space-x-0 lg:space-x-0 min-w-100 h-24 bg-red-500 px-5">
-            <div>
-                <img src="https://images.squarespace-cdn.com/content/v1/5939fcd1db29d6ec60929205/1599605891670-HLWDP9UQBSK6XT6DLF3A/SGA+White+Text+Transparent.png%3Fformat=1500w" alt="Student Government Association Logo" className="w-32"/>
-            </div>
+	return (
+		<div className="flex items-center justify-between space-x-16 md:space-x-0 lg:space-x-0 min-w-100 h-24 bg-red-500 px-5">
+			<div>
+				<img
+					src="https://images.squarespace-cdn.com/content/v1/5939fcd1db29d6ec60929205/1599605891670-HLWDP9UQBSK6XT6DLF3A/SGA+White+Text+Transparent.png%3Fformat=1500w"
+					alt="Student Government Association Logo"
+					className="w-32"
+				/>
+			</div>
 
-            <ul className="flex items-center justify-around space-x-8">
-                {
-                navigation.map((elem: string) => {
-                    return <li key={elem}><a href="" className="text-white hover:text-black hidden sm:block sm:text-xl">{elem}</a></li>
-                })
-                }
-            </ul>
+			<ul className="flex items-center justify-around space-x-8">
+				{navigation.map((elem: string) => {
+					return (
+						<li key={elem}>
+							<a
+								href=""
+								className="text-white hover:text-black hidden sm:block sm:text-xl"
+							>
+								{elem}
+							</a>
+						</li>
+					);
+				})}
+			</ul>
 
-            
-            <svg aria-label="menu icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-12 h-12 sm:hidden">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>
-        
-        </div>
-    );
-}
+			<svg
+				aria-label="menu icon"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				strokeWidth={1.5}
+				stroke="white"
+				className="w-12 h-12 sm:hidden"
+			>
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+				/>
+			</svg>
+		</div>
+	);
+};
 
 export default Menu;
