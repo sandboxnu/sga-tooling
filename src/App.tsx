a1857772e89989a0802b692e897333e2460ff373
Ally Chao <73146562+allychao@users.noreply.github.com>
2022-11-04 16:56:39 -0400

    add desktop support

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,30 @@ import EventCard from "./components/EventCard";
 function App() {
   return (
     <>
-      <Menu />
-      <div className="flex flex-row">
-        <div className="w-1/5">placeholder for dates whee</div>
-        <EventCard
-          startTime={new Date()}
-          name="Sample Event"
-          location="WVF 020"
-          description="Sample text. Please don't read this area. If you do you may be subject to legal action."
-          live={false}
-        />
+      <div className="flex min-h-screen flex-col justify-start">
+        <Menu />
+        <div className="flex flex-row">
+          <div className="w-1/5 md:w-[10%]">placeholder for dates whee</div>
+          <EventCard
+            startTime={new Date()}
+            name="Sample Event 1"
+            location="WVF 020"
+            description="Sample text. Please don’t read this area. If you do you may be subject to legal action. The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal."
+            live={false}
+          />
+        </div>
+        <div className="flex flex-row">
+          <div className="w-1/5 md:w-[10%]">this be the live event</div>
+          <EventCard
+            startTime={new Date()}
+            name="Sample Event 2"
+            location="Afterhours, Curry Student Center"
+            description="The organizer of this event has chosen to notify you about it. The agenda is to vibe to Lil Nas X."
+            live={true}
+          />
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 }
--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -40,12 +40,12 @@ const EventCard = ({
 
   return (
     <>
-      <div className="pl-4 pt-4 pr-6 w-4/5">
-        <span className="font-sans">
+      <div className="pl-4 pt-4 pr-6 w-4/5 md:w-[90%]">
+        <span className="font-sans md:text-lg">
           {startTimeString + (endTime ? " to " + endTimeString : "")}
         </span>
         <div className="flex flex-row justify-between items-start mb-4">
-          <div className="not-italic font-bold text-2xl leading-8 font-sans break-words w-4/5">
+          <div className="not-italic font-bold text-2xl leading-8 font-sans break-words w-4/5 md:pr-64 md:text-[22px]">
             {name}
           </div>
           <img
@@ -54,28 +54,30 @@ const EventCard = ({
             aria-label="Open Event Card details"
           />
         </div>
-        <div className="flex flex-row items-start mb-4">
+        <div className="flex flex-row items-start mb-4 md:ml-4">
           <img src={PinSVG} alt="Pin svg" className="p-1 pt-0" />
-          <span className="text-body-mobile pl-2 pr-12 pt-0.5 font-montserrat break-words w-full">
+          <span className="text-body-mobile pl-2 pr-8 pt-0.5 font-montserrat break-words w-full md:pr-64 md:text-lg">
             {location}
           </span>
         </div>
-        <div className="flex flex-row items-start mb-6">
+        <div className="flex flex-row items-start mb-6 md:ml-4">
           <img src={TextIconSVG} alt="TextIcon svg" className="p-1 pt-0" />
-          <p className="text-body-mobile pl-2 pr-12 font-montserrat break-words w-full">
+          <p className="text-body-mobile pl-2 pr-8 font-montserrat break-words w-full md:pr-64 md:text-lg">
             {description}
           </p>
         </div>
 
-        <div className="flex flex-row flex-wrap">
+        <div className="flex flex-row flex-wrap justify-end">
           {live ? (
-            <button className="button-base-red px-4 my-2">Vote</button>
+            <button className="button-base-red px-4 my-2 md:px-6">Vote</button>
           ) : (
             <>
-              <button className="button-base-white px-2 my-2 mr-5">
+              <button className="button-base-white px-2 my-2 mr-5 md:mr-10 md:px-5">
                 Unregister
               </button>
-              <button className="button-base-red px-4 my-2">See More</button>
+              <button className="button-base-red px-4 my-2 md:px-7">
+                See More
+              </button>
             </>
           )}
         </div>
