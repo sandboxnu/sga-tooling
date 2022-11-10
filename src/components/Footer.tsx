ff23504ba03511fd3c6840e11b57dde5c139ac32
Ally Chao <73146562+allychao@users.noreply.github.com>
2022-11-10 18:41:25 -0500

    revert desktop changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { ReactElement } from "react";
-import ".././styles.css";
+import '.././styles.css';
 
 // Footer with source code and project information that goes at the bottom of each page
 const Footer = (): ReactElement => {
   return (
-    <div className="w-full px-2 py-4 mt-4 flex flex-row justify-between border-t-2 bottom-0 mt-auto">
+    <div className="w-full px-2 py-4 mt-4 flex flex-row justify-between border-t-2 absolute bottom-0">
       <div>
         Made by students @{" "}
-        <a
-          className="link"
+        <a className="link"
           onClick={() => {
             window.open("https://sandboxnu.com");
           }}
@@ -17,8 +16,7 @@ const Footer = (): ReactElement => {
           Sandbox
         </a>
         . Source on{" "}
-        <a
-          className="link"
+        <a className="link"
           onClick={() => {
             window.open("https://github.com/sandboxnu/sga-tooling");
           }}
@@ -28,8 +26,7 @@ const Footer = (): ReactElement => {
       </div>
 
       <div>
-        <a
-          className="link"
+        <a className="link"
           onClick={() => {
             window.open(
               "https://github.com/sandboxnu/office-hours/discussions"
@@ -39,8 +36,7 @@ const Footer = (): ReactElement => {
           Give us feedback
         </a>
         <span> or </span>
-        <a
-          className="link"
+        <a className="link"
           onClick={() => {
             window.open("https://github.com/sandboxnu/office-hours/issues");
           }}
