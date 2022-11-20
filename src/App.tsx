fe8e8d83c582ab17097a436510ad0e8182a13950
chanmi <40529597+chanmioh@users.noreply.github.com>
2022-11-20 18:01:30 -0500

    [ST-28] Homepage with Events & Dates (#11)
    Added homepage with event cards and dates next to them

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,53 @@
-import React, { createContext, useEffect, useState } from 'react';
-import {
-  BrowserRouter as Router, Route, Routes
-} from "react-router-dom";
-import './App.css';
-import Alert from './components/Alert';
-import Error404 from './components/Error404';
-import Footer from './components/Footer';
-import LoginPage from './components/LoginPage';
-import Menu from './components/Menu';
+import React, { createContext, useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Alert from "./components/Alert";
+import Error404 from "./components/Error404";
+import Footer from "./components/Footer";
+import LoginPage from "./components/LoginPage";
+import Menu from "./components/Menu";
+import Homepage from "./Homepage";
 
 export type User = string | null;
 
 type UserContext = {
-  user: User,
-  setUser: React.Dispatch<React.SetStateAction<User>>,
+  user: User;
+  setUser: React.Dispatch<React.SetStateAction<User>>;
 };
 
 export const LoginContext = createContext<UserContext>({} as UserContext);
 
 function App() {
-
-  const [user, setUser] = useState<User | null>(null)
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     const nuid = localStorage.getItem("user");
     if (nuid) {
-      setUser(nuid)
+      setUser(nuid);
     }
-    
-  }, [])
+  }, []);
 
   return (
-      <LoginContext.Provider value={{user, setUser}}>
-        <div className="flex min-h-screen flex-col justify-between">
-          {user ? <Menu /> : null}
-          <Router>
-            <Routes>
-                <Route path="/" element={<LoginPage />} errorElement={<Error404 />} />
-                <Route path="/events" element={<></>}>
-                  {/* :alerID needs to be updated to display Events */}
-                  <Route path=":alertID" element={<Alert message='hi'/>} />
-                </Route>
-                <Route path="*" element={<Error404 />} />
-            </Routes>
-          </Router>
-          <Footer />
-        </div>
-      </LoginContext.Provider>
+    <LoginContext.Provider value={{ user, setUser }}>
+      <div className="flex min-h-screen flex-col justify-between">
+        {user ? <Menu /> : null}
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={<LoginPage />}
+              errorElement={<Error404 />}
+            />
+            <Route path="/events" element={<Homepage />}>
+              <Route path=":alertID" element={<Alert message="hi" />} />
+              {/* alertID needs to be updated to display events */}
+            </Route>
+            <Route path="*" element={<Error404 />} />
+          </Routes>
+        </Router>
+        <Footer />
+      </div>
+    </LoginContext.Provider>
   );
 }
 
---
+++ src/Homepage.tsx
@@ -0,0 +1,70 @@
+import { ReactElement } from "react";
+import SearchIcon from "./assets/SearchIcon.svg";
+import Alert from "./components/Alert";
+import EventCard, { Event } from "./components/EventCard";
+import EventsJSON from "./events.json";
+
+// Renders homepage with events.
+const Homepage = (): ReactElement => {
+  function getColor(start: Date, end: Date) {
+    const today = new Date();
+    const timeNow = today.getTime();
+
+    if (start.getTime() < timeNow && timeNow < end.getTime()) {
+      return "bg-sga-red";
+    } else if (today.toDateString() === start.toDateString()) {
+      return "bg-black";
+    } else {
+      return "bg-white";
+    }
+  }
+
+  const events: Event[] = EventsJSON.map((e) => {
+    return {
+      startTime: new Date(e.startTime),
+      endTime: new Date(e.endTime),
+      name: e.name,
+      location: e.location,
+      description: e.description,
+      color: getColor(new Date(e.startTime), new Date(e.endTime)),
+    };
+  });
+
+  const liveEvents = events
+    .filter((e) => e.color === "bg-sga-red")
+    .map((e) => (
+      <>
+        <EventCard {...e} />
+        <hr className="border-black home-mx" />
+      </>
+    ));
+
+  const upcomingEvents = events
+    .filter((e) => e.color !== "bg-sga-red")
+    .map((e) => (
+      <>
+        <EventCard {...e} />
+        <hr className="border-black home-mx" />
+      </>
+    ));
+
+  return (
+    <>
+      <h1 className="section-heading">HAPPENING NOW</h1>
+      {liveEvents}
+
+      <Alert
+        message="Your standing in SGA may be affected if you miss the next event."
+        className="home-mx mt-5"
+      />
+
+      <div className="section-heading flex justify-between items-center">
+        <h1>UPCOMING EVENTS</h1>
+        <img src={SearchIcon} aria-label="Search for an event"></img>
+      </div>
+      {upcomingEvents}
+    </>
+  );
+};
+
+export default Homepage;
