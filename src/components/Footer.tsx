f1db6702f202bdfcdb19c301f7e98cb175a6bb92
Ally Chao <73146562+allychao@users.noreply.github.com>
2022-11-28 20:43:05 -0500

    initial footer changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,48 +1,28 @@
 import { ReactElement } from "react";
+import ChevronRightSVG from ".././assets/ChevronRight.svg";
 import ".././styles.css";
 
 // Footer with source code and project information that goes at the bottom of each page
 const Footer = (): ReactElement => {
   return (
-    <div className="w-full px-2 py-4 mt-4 flex flex-row justify-between border-t-2 bottom-0">
-      <div>
-        Made by students @&nbsp;
-        <a
-          href="https://sandboxnu.com"
-          className="link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Sandbox
-        </a>
-        . Source on&nbsp;
-        <a
-          href="https://github.com/sandboxnu/sga-tooling"
-          className="link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub.
-        </a>
+    <div className="w-full px-6 py-4 mt-4 flex flex-row justify-between bottom-0 bg-sga-red md:py-6 md:px-8">
+      <div className="font-montserrat text-base font-bold text-white">
+        Made by Sandbox
       </div>
 
       <div>
         <a
           href="https://github.com/sandboxnu/office-hours/discussions"
-          className="link"
+          className="font-montserrat text-base font-medium text-white flex flex-row"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Give us feedback
-        </a>
-        <span> or </span>
-        <a
-          href="https://github.com/sandboxnu/office-hours/issues"
-          className="link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          File a bug report
+          Learn More
+          <img
+            src={ChevronRightSVG}
+            alt="ChevronRight svg"
+            aria-label="Learn More About Sandbox"
+          />
         </a>
       </div>
     </div>
