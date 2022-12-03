a912683bb70fe4536932611aee0d4f574732d8d4
chanmi <40529597+chanmioh@users.noreply.github.com>
2022-12-02 23:18:04 -0500

    Added /pages folder, cleaned up assets, and replaced filter() with reduce() (#13)
    * Added /pages folder, cleaned up assets, and replaced filter function with reduce
    
    * Moved divider, added isSameDay function
    
    * Hide black line for same day events
    
    * Renamed ErrorIcon svg

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -5,13 +5,19 @@ import TextIconSVG from ".././assets/TextIcon.svg";
 import ".././styles.css";
 import { EventDate } from "./EventDate";
 
+export enum Status {
+  Live = "bg-sga-red",
+  Today = "bg-black",
+  Upcoming = "bg-white",
+}
+
 export type Event = {
   startTime: Date;
   endTime: Date;
   name: string;
   location: string;
   description: string;
-  color: string;
+  status: Status;
 };
 
 /**
@@ -23,7 +29,7 @@ const EventCard = ({
   name,
   location,
   description,
-  color,
+  status,
 }: Event): ReactElement => {
   let startTimeString: string = startTime.toLocaleString("en-US", {
     hour: "numeric",
@@ -39,51 +45,51 @@ const EventCard = ({
     : undefined;
 
   return (
-    <div className="flex my-8 md:my-10">
-      <EventDate startTime={startTime} color={color} />
-      <div className="flex-1 px-6 md:px-10">
-        <span className="font-sans">
-          {startTimeString + (endTime ? " to " + endTimeString : "")}
-        </span>
-        <div className="flex flex-row justify-between items-start mb-4 md:mb-2">
-          <div className="not-italic font-bold text-2xl leading-8 font-sans break-words w-4/5 md:pr-64">
-            {name}
-          </div>
-          <img
-            src={MeatballMenuSVG}
-            alt="Menu svg"
-            aria-label="Open Event Card details"
-          />
-        </div>
-        <div className="flex flex-row items-start mb-4 md:ml-4 md:mb-2">
-          <img src={PinSVG} alt="Pin svg" className="p-1 pt-0" />
-          <span className="text-body-mobile pl-2 pr-8 pt-0.5 font-montserrat break-words w-full md:pr-64">
-            {location}
+    <>
+      <div className="flex my-8 md:my-10">
+        <EventDate startTime={startTime} status={status} />
+        <div className="flex-1 px-6 md:px-10">
+          <span className="font-sans">
+            {startTimeString + (endTime ? " to " + endTimeString : "")}
           </span>
-        </div>
-        <div className="flex flex-row items-start mb-6 md:ml-4 md:mb-0">
-          <img src={TextIconSVG} alt="TextIcon svg" className="p-1 pt-0" />
-          <p className="text-body-mobile pl-2 pr-8 font-montserrat break-words w-full md:pr-64">
-            {description}
-          </p>
-        </div>
+          <div className="flex flex-row justify-between items-start mb-4">
+            <div className="not-italic font-bold text-2xl leading-8 font-sans break-words w-4/5">
+              {name}
+            </div>
+            <img
+              src={MeatballMenuSVG}
+              alt="Menu svg"
+              aria-label="Open Event Card details"
+            />
+          </div>
+          <div className="flex flex-row items-start mb-4">
+            <img src={PinSVG} alt="Pin svg" className="p-1 pt-0" />
+            <span className="text-body-mobile pl-2 pr-12 pt-0.5 font-montserrat break-words w-full">
+              {location}
+            </span>
+          </div>
+          <div className="flex flex-row items-start mb-6">
+            <img src={TextIconSVG} alt="TextIcon svg" className="p-1 pt-0" />
+            <p className="text-body-mobile pl-2 pr-12 font-montserrat break-words w-full">
+              {description}
+            </p>
+          </div>
 
-        <div className="flex flex-row flex-wrap justify-end md:justify-start md:mt-4">
-          {color === "bg-sga-red" ? (
-            <button className="button-base-red px-4 my-2 md:px-6">Vote</button>
-          ) : (
-            <>
-              <button className="button-base-white px-2 my-2 mr-5 md:mr-4 md:px-5">
-                Unregister
-              </button>
-              <button className="button-base-red px-4 my-2 md:px-7">
-                See More
-              </button>
-            </>
-          )}
+          <div className="flex flex-row flex-wrap">
+            {status === Status.Live ? (
+              <button className="button-base-red px-4 my-2">Vote</button>
+            ) : (
+              <>
+                <button className="button-base-white px-2 my-2 mr-5">
+                  Unregister
+                </button>
+                <button className="button-base-red px-4 my-2">See More</button>
+              </>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
