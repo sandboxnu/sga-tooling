33f2f1b89596c7b23d19fbaa04f43e0a8b1176da
SuliR123 <solomonrashidzada@gmail.com>
2023-02-12 15:27:02 -0500

    Redirects not logged in users

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,16 @@ import Menu from "./components/Menu";
 import Error404 from "./pages/Error404";
 import Homepage from "./pages/Homepage";
 import LoginPage from "./pages/LoginPage";
-import Settings from "./components/Settings"; 
+import RequireAuth from "./components/RequireAuth";
 
-export type User = string | null;
+export type User = string | null; 
 
 type UserContext = {
   user: User;
-  setUser: React.Dispatch<React.SetStateAction<User>>;
+  setUser: React.Dispatch<React.SetStateAction<User>>; 
 };
 
-export const LoginContext = createContext<UserContext>({} as UserContext);
+export const LoginContext = createContext<UserContext>({} as UserContext); 
 
 function App() {
   const [user, setUser] = useState<User | null>(null);
@@ -31,18 +31,21 @@ function App() {
   return (
     <LoginContext.Provider value={{ user, setUser }}>
       <div className="flex min-h-screen flex-col justify-between">      
-        <Router>
-        {user ? <Menu /> : null}
+        <Router> 
+        {user ? <Menu /> : null} 
           <Routes>
             <Route
               path="/"
               element={<LoginPage />}
               errorElement={<Error404 />}
             />
-            <Route path="/events" element={<Homepage />}>
-              <Route path=":alertID" element={<Alert message="hi" />} />
-              {/* alertID needs to be updated to display events */}
-            </Route>
+            <Route element={<RequireAuth />}>
+              <Route path="/events" element={<Homepage />}>
+                <Route path=":alertID" element={<Alert message="hi" />} />
+                {/* alertID needs to be updated to display events */}
+              </Route>
+            </Route> 
+            
             <Route path="*" element={<Error404 />} /> 
           </Routes>
         </Router>
