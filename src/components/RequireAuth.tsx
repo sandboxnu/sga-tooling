4650d50e0576954dfdaa805d42fee190eb383fc1
Dylan Cerenov <dcerenov25@gmail.com>
2023-02-15 21:34:45 -0500

    finishing the commit thingy

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,9 +1,11 @@
 import { ReactElement, useContext } from "react";
-import { LoginContext } from "../App";
 import { Navigate, Outlet } from "react-router-dom";
+import { LoginContext } from "../App";
 
-const RequireAuth = (): ReactElement => {    
-    const {user} = useContext(LoginContext);
+// Navigation to the login menu if the user is not logged in. 
+
+const RequireAuth = (): ReactElement => {
+    const { user } = useContext(LoginContext);
     return user ? <Outlet /> : <Navigate to="/" />; //yuh 
 }
 
