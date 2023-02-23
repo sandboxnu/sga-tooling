024cce1a25c20e21057995febcf4cfd453528145
chanmioh <chanmioh@gmail.com>
2023-02-22 20:46:54 -0500

    Fixed tailwing config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const { url } = require('inspector');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
@@ -13,23 +11,20 @@ module.exports = {
         "alert-yellow": "#FFEDC0",
         "tag-blue": "#8DD8E6",
         "tag-green": "#88BB77",
-
       },
       backgroundImage: {
         "cooper-mobile": "url(../src/assets/cooper-mobile.jpg)",
         "cooper-mobile-festive": "url(../src/assets/cooper-mobile-festive.png)",
         "cooper-big-boy": "url(../src/assets/cooper-big-boy.jpg)",
+        isec: "url('../src/assets/Isec.png')",
       },
       fontFamily: {
         montserrat: ["Montserrat"],
         sans: ['"Open Sans"'],
       },
       dropShadow: {
-        'event-dropdown': '0 12px 12px rgba(0, 0, 0, 0.3)',
+        "event-dropdown": "0 12px 12px rgba(0, 0, 0, 0.3)",
       },
-      backgroundImage: {
-        'isec': "url('../src/assets/Isec.png')",
-      }
     },
   },
   plugins: [],
