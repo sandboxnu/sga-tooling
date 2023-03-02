45321f2be067daa95874cf3437e4837230d620a6
justagoodfriend <greenate1@gmail.com>
2023-03-02 08:24:08 -0500

    removed fields not related to members

--- src/pages/UserPreference.tsx
+++ src/pages/UserPreference.tsx
@@ -27,24 +27,13 @@ const UserPreference = ({ member }: UserPreferenceProp): ReactElement => {
 
       <div className="flex flex-col px-7 font-sans gap-8">
         <span className="font-bold text-xl">CONTACT INFO</span>
-        <div className="flex gap-x-12">
-          <div>
-            <span className="text-gray-600">Name</span> <br />
-            {member.firstName + " " + member.lastName}
-          </div>
-          <div>
-            <span className="text-gray-600">Pronouns</span> <br />
-            <span> Huskies/Rock</span>
-          </div>
+        <div>
+          <span className="text-gray-600">Name</span> <br />
+          {member.firstName + " " + member.lastName}
         </div>
         <div className="flex flex-col">
           <span className="text-gray-600">Email</span>
           <span>{member.email}</span>
-        </div>
-
-        <div className="flex flex-col">
-          <span className="text-gray-600">Phone Number</span>
-          <span> +1 (911) 911 - 1911</span>
         </div>
 
         <hr className="border-black home-mx -ml-7 -mr-7" />
