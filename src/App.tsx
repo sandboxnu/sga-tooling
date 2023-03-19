e6483b42fcbe644c330cf064f2143410f55a67a4
Melody Liu <33408946+melocule@users.noreply.github.com>
2023-03-19 16:06:38 -0400

    Add back the event details page and remove some comments (#33)

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,31 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Alert from "./components/Alert";
 import Footer from "./components/Footer";
 import Menu from "./components/Menu";
 import RequireAuth from "./components/RequireAuth";
 import Error404 from "./pages/Error404";
+import EventDetailsPage from "./pages/EventDetailsPage";
 import Homepage from "./pages/Homepage";
 import LoginPage from "./pages/LoginPage";
 
-export type User = string | null;
+export type UserID = string | null;
 
 type UserContext = {
-  user: User;
-  setUser: React.Dispatch<React.SetStateAction<User>>;
+  userID: UserID;
+  setUserID: React.Dispatch<React.SetStateAction<UserID>>;
 };
 
-export const LoginContext = createContext<UserContext>({} as UserContext);
+export const LoginContext = createContext<UserContext>({ userID: null, setUserID: () => { } });
 
 function App() {
-  const [user, setUser] = useState<User | null>(null);
-
-  useEffect(() => {
-    const nuid = localStorage.getItem("user");
-    if (nuid) {
-      setUser(nuid);
-    }
-  }, []);
+  const [userID, setUserID] = useState<UserID>(localStorage.getItem("user"));
 
   return (
-    <LoginContext.Provider value={{ user, setUser }}>
+    <LoginContext.Provider value={{ userID, setUserID }}>
       <div className="flex min-h-screen flex-col justify-between">
         <Router>
-          {user ? <Menu /> : null}
+          {userID ? <Menu /> : null}
           <Routes>
             <Route
               path="/"
@@ -40,16 +33,14 @@ function App() {
               errorElement={<Error404 />}
             />
             <Route element={<RequireAuth />}>
-              <Route path="/events" element={<Homepage />}>
-                <Route path=":alertID" element={<Alert message="hi" />} />
-                {/* alertID needs to be updated to display events */}
-              </Route>
+              <Route path="/events" element={<Homepage />} />
+              <Route path="/events/:id" element={<EventDetailsPage />} />
             </Route>
 
             <Route path="*" element={<Error404 />} />
           </Routes>
         </Router>
-        {user ? <Footer hideInfo={false} /> : <Footer hideInfo={true} />}
+        {userID ? <Footer hideInfo={false} /> : <Footer hideInfo={true} />}
       </div>
     </LoginContext.Provider>
   );
