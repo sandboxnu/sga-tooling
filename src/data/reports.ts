3174d7f10efa200b0cab2756fc3825d8bffe0527
justagoodfriend <greenate1@gmail.com>
2023-04-12 21:16:13 -0400

    idk some change

--- src/data/reports.ts
+++ src/data/reports.ts
@@ -11,4 +11,47 @@ export const mockReports: Report[] = [
     resolution_action: new Date("2000-01-01 00:00:00"),
     resolution_time: new Date("2000-01-01 00:00:00"),
   },
+  {
+    id: 1,
+    member_id: 1,
+    reported_time: new Date("2000-01-01 00:00:00"),
+    report_reason: "something",
+    event_id: 2,
+    request_id: 3,
+    resolution_action: new Date("2000-01-01 00:00:00"),
+    resolution_time: new Date("2000-01-01 00:00:00"),
+  },
+
+  {
+    id: 1,
+    member_id: 1,
+    reported_time: new Date("2000-01-01 00:00:00"),
+    report_reason: "something",
+    event_id: 3,
+    request_id: 3,
+    resolution_action: new Date("2000-01-01 00:00:00"),
+    resolution_time: new Date("2000-01-01 00:00:00"),
+  },
+
+  {
+    id: 1,
+    member_id: 1,
+    reported_time: new Date("2000-01-01 00:00:00"),
+    report_reason: "something",
+    event_id: 4,
+    request_id: 3,
+    resolution_action: new Date("2000-01-01 00:00:00"),
+    resolution_time: new Date("2000-01-01 00:00:00"),
+  },
+
+  {
+    id: 1,
+    member_id: 1,
+    reported_time: new Date("2000-01-01 00:00:00"),
+    report_reason: "something",
+    event_id: 1,
+    request_id: 3,
+    resolution_action: new Date("2000-01-01 00:00:00"),
+    resolution_time: new Date("2000-01-01 00:00:00"),
+  },
 ];
