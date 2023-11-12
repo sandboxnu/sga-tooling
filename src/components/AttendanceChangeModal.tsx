cf0325fffd373b65962b2b114e7a1f3465ca920b
Nathan Harvey <115117835+justagoodfriend@users.noreply.github.com>
2023-11-12 16:13:12 -0500

    Sf2 17 Implement Attendance Change Modal Design (#45)
    * start of Desktop Attendance Modal and little of Mobile
    
    * adding back functionality for the modal
    
    * error validation and diasble scrolling
    
    * pr comments and fixing weird case with clearing field
    
    * making select wider on mobile
    
    * Updates validation logic
    
    * using switch statment
    
    ---------
    
    Co-authored-by: Raveesh Mehta <mehta.r@northeastern.edu>

--- src/components/AttendanceChangeModal.tsx
+++ src/components/AttendanceChangeModal.tsx
@@ -14,16 +14,32 @@ const AttendanceChangeModal = ({
   setAttendanceChange,
 }: AttendanceChangeModalProps) => {
   const [reason, setReason] = useState<string>("");
-  const [requestType, setRequestType] = useState<RequestType>(
-    RequestType.ABSENT
+  const [requestType, setRequestType] = useState<RequestType | undefined>(
+    undefined
   );
   const [lateArrivalTime, setLateArrivalTime] = useState<Date | null>(null);
   const [earlyDepartureTime, setEarlyDepatureTime] = useState<Date | null>(
     null
   );
+  const [error, setError] = useState<boolean>(false);
+
+  const depatureTimeEnabled =
+    requestType === RequestType.LEAVING_EARLY ||
+    requestType === RequestType.BOTH;
+
+  const arrivalTimeEnabled =
+    requestType === RequestType.ARRIVING_LATE ||
+    requestType === RequestType.BOTH;
 
   const requestOptionHandler = (event: React.FormEvent<HTMLSelectElement>) => {
     const eventValue: RequestType = event.currentTarget.value as RequestType;
+    if (requestType === RequestType.BOTH) {
+      if (eventValue === RequestType.ARRIVING_LATE) setEarlyDepatureTime(null);
+      if (eventValue === RequestType.LEAVING_EARLY) setLateArrivalTime(null);
+    } else if (eventValue !== RequestType.BOTH) {
+      setEarlyDepatureTime(null);
+      setLateArrivalTime(null);
+    }
     setRequestType(eventValue);
   };
 
@@ -31,7 +47,24 @@ const AttendanceChangeModal = ({
     { value: RequestType.ARRIVING_LATE, label: "Arrive Late" },
     { value: RequestType.LEAVING_EARLY, label: "Leave Early" },
     { value: RequestType.ABSENT, label: "Absent" },
+    { value: RequestType.BOTH, label: "Arrive Late and Leave Early" },
   ];
+
+  const validateSubmission = (submission: AttendanceData) => {
+    if (!submission.reason || submission.request_type === undefined) {
+      return false;
+    }
+    switch (submission.request_type) {
+      case RequestType.ARRIVING_LATE:
+        return submission.time_arriving;
+      case RequestType.LEAVING_EARLY:
+        return submission.time_leaving;
+      case RequestType.BOTH:
+        return submission.time_arriving && submission.time_leaving;
+      default:
+        return false;
+    }
+  };
 
   const submitForm = () => {
     const submissonJson: AttendanceData = {
@@ -48,72 +81,163 @@ const AttendanceChangeModal = ({
       submissonJson.time_leaving = earlyDepartureTime;
     }
 
-    setAttendanceChange(submissonJson);
-    onClose();
+    const isValid = validateSubmission(submissonJson);
+
+    if (isValid) {
+      setAttendanceChange(submissonJson);
+      onClose();
+    } else {
+      setError(true);
+    }
   };
 
   const resetFields = () => {
     setReason("");
-    setRequestType(RequestType.ABSENT);
+    setRequestType(undefined);
     setEarlyDepatureTime(null);
     setLateArrivalTime(null);
+    setError(false);
     onClose();
   };
 
   return isOpen ? (
-    <div className="fixed top-0 right-0 bottom-0 left-0 m-auto bg-[hsla(0,0%,0%,.5)] h-screen w-screen flex justify-center items-center z-50">
-      <div className="bg-white">
-        <div className="border-b border-solid border-black">
-          <span>Submit Attendance Form</span>
-          <button className="float-right">
-            {" "}
-            <XMarkIcon onClick={() => resetFields()} className="w-5" />{" "}
-          </button>
-        </div>
-        <form onSubmit={submitForm}>
-          <div className="flex flex-col justify-center items-center mx-20">
-            <select onChange={requestOptionHandler}>
-              <option>Please choose one option</option>
-              {availableOptions.map((option, index) => {
-                return (
-                  <option value={option.value} key={index}>
-                    {option.label}
-                  </option>
-                );
-              })}
-            </select>
-            {/* Let Designers Figure out what would be the best way to get a time input */}
-            <div>
-              <label>Describe Reason for Filling out form</label>
-              <input
-                style={{ border: "1px solid" }}
-                type="text"
-                onChange={(e) => setReason(e.target.value)}
-              />
-            </div>
-            {requestType === RequestType.ARRIVING_LATE && (
-              <div>
-                <label>What will be your new arrival time?</label>
-                <input
-                  style={{ border: "1px solid" }}
-                  type="time"
-                  onChange={(e) => setLateArrivalTime(e.target.valueAsDate)}
-                />
+    <div className="fixed top-0 right-0 bottom-0  left-0 m-auto bg-[hsla(0,0%,0%,.5)] h-screen w-screen flex justify-center items-center z-50">
+      <div
+        className={`flex bg-white rounded-3xl max-w-2xl text-sm ${
+          error ? "border-2 border-red-600" : ""
+        }`}
+      >
+        <div>
+          <XMarkIcon
+            onClick={() => resetFields()}
+            role="button"
+            className="h-10 w-10 float-right mt-4 mr-4"
+          />
+          <div className="flex flex-col justify-center items-center pl-14 pt-16 font-montserrat gap-4">
+            <span className="font-bold text-2xl">Attendance Form</span>
+            <span className="flex text-center">
+              SGA members must indicate their attendance for events ahead of
+              time and are expected to attend unless an excused reason is
+              submitted and approved.
+            </span>
+
+            <span className="font-bold text-base text-center">
+              Describe reason for filling out form
+            </span>
+
+            <form
+              onFocus={() => setError(false)}
+              onSubmit={(e) => {
+                submitForm();
+                e.preventDefault();
+              }}
+            >
+              <div className="flex flex-col items-center sm:flex-row gap-2">
+                <div className="flex flex-col pr-2 w-full">
+                  <label htmlFor="option">Request</label>
+                  <select
+                    id="option"
+                    onChange={requestOptionHandler}
+                    className="border border-solid border-black rounded-md py-2"
+                  >
+                    <option>Please choose one option</option>
+                    {availableOptions.map((option, index) => {
+                      return (
+                        <option value={option.value} key={index}>
+                          {option.label}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+
+                <div className="flex gap-2">
+                  <div className="flex flex-col">
+                    <label
+                      htmlFor="arrivalTime"
+                      className={` ${
+                        arrivalTimeEnabled ? "text-gray-500" : "text-black"
+                      }`}
+                    >
+                      {" "}
+                      Arrival Time
+                    </label>
+                    {arrivalTimeEnabled ? (
+                      <input
+                        className="rounded-md py-2 w-32 border border-solid border-black"
+                        id="arrivalTime"
+                        type={"time"}
+                        onChange={(e) =>
+                          setLateArrivalTime(e.target.valueAsDate)
+                        }
+                      ></input>
+                    ) : (
+                      <input
+                        className="rounded-md py-2 bg-gray-300 w-32 border border-solid border-black"
+                        id="arrivalTime"
+                        disabled
+                        value=""
+                      ></input>
+                    )}
+                  </div>
+
+                  <div className="w-5 border h-0.5 self-center border-black bg-black mt-4" />
+
+                  <div className="flex flex-col">
+                    <label
+                      htmlFor="depatureTime"
+                      className={` ${
+                        depatureTimeEnabled ? "text-gray-500" : "text-black"
+                      }`}
+                    >
+                      {" "}
+                      Departure Time{" "}
+                    </label>
+                    {depatureTimeEnabled ? (
+                      <input
+                        className="rounded-md py-2 w-32 border border-solid border-black"
+                        id="depatureTime"
+                        type={"time"}
+                        onChange={(e) =>
+                          setEarlyDepatureTime(e.target.valueAsDate)
+                        }
+                      ></input>
+                    ) : (
+                      <input
+                        className="rounded-md py-2 bg-gray-300 w-32  border border-solid border-black"
+                        id="depatureTime"
+                        disabled
+                        value={""}
+                      ></input>
+                    )}
+                  </div>
+                </div>
               </div>
-            )}
-            {requestType === RequestType.LEAVING_EARLY && (
-              <div>
-                <label>What will be your new departure time?</label>
-                <input
-                  style={{ border: "1px solid" }}
-                  type="time"
-                  onChange={(e) => setEarlyDepatureTime(e.target.valueAsDate)}
-                />
+              {error && (
+                <div className="flex justify-center">
+                  <p className={"text-red-600 pt-2 text-center"}>
+                    Please input required fields before submission
+                  </p>
+                </div>
+              )}
+
+              <div className="flex flex-col py-2">
+                <div>
+                  <label htmlFor="Reason">Reason for request</label>
+                  <textarea
+                    id="Reason"
+                    className="resize-none border border-solid rounded-md w-full h-40 border-black"
+                    onChange={(e) => setReason(e.target.value)}
+                  ></textarea>
+                </div>
+                <button className="text-white self-center button-base-red px-4 my-2 w-32 rounded-md font-sans font-bold py-2">
+                  {" "}
+                  Submit
+                </button>
               </div>
-            )}
+            </form>
           </div>
-          <button type="submit">Submit</button>
-        </form>
+        </div>
       </div>
     </div>
   ) : (
